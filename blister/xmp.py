960520b723d1af1999c647ebea8969b4837aa458
Matthew Alexander LaChance <mattlach@umich.edu>
2016-09-13 08:46:22 -0400

    Write minimal code to implement MutableMapping

--- blister/xmp.py
+++ blister/xmp.py
@@ -1,6 +1,20 @@
 # Copyright (c) 2016 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
+from collections.abc import MutableMapping
 
-class VanillaXMP:
-    pass
+class VanillaXMP (MutableMapping):
+    def __delitem__ (self, key):
+        pass
+
+    def __getitem__ (self, key):
+        pass
+
+    def __iter__ (self):
+        pass
+
+    def __len__ (self):
+        pass
+
+    def __setitem__ (self, key, value):
+        pass
