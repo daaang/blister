92d7ebea061f853b29cec264413d58bfe0950af5
Matthew Alexander LaChance <mattlach@umich.edu>
2016-09-23 11:20:45 -0400

    Add URI subtype of str

--- blister/test/test_xmp.py
+++ blister/test/test_xmp.py
@@ -4,10 +4,39 @@
 from collections.abc import MutableMapping
 from unittest import TestCase
 
-from blister.xmp import VanillaXMP
+from blister.xmp import VanillaXMP, URI
 
 class TestVanillaXMP (TestCase):
 
     def test_is_mutable_mapping (self):
         xmp = VanillaXMP()
         self.assertTrue(isinstance(xmp, MutableMapping))
+
+class TestURI (TestCase):
+
+    def setUp (self):
+        self.eg_str = "meaningless example"
+        self.eg_uri = URI("meaningless example")
+
+    def test_is_str_instance (self):
+        self.assertTrue(isinstance(self.eg_uri, str))
+
+    def test_str_is_not_uri (self):
+        self.assertFalse(isinstance(self.eg_str, URI))
+
+    def test_equal_to_str (self):
+        self.assertEqual(self.eg_str, self.eg_uri)
+        self.assertEqual(self.eg_uri, self.eg_str)
+
+    def test_hash_into_dict (self):
+        d = { }
+        d[URI("a")] = "hi"
+
+        self.assertIn("a", d)
+        self.assertIn(URI("a"), d)
+
+        self.assertEqual(d["a"], "hi")
+
+    def test_repr (self):
+        self.assertEqual(repr(self.eg_uri),
+                "<URI {}>".format(self.eg_str))
--- blister/xmp.py
+++ blister/xmp.py
@@ -3,7 +3,40 @@
 # BSD License. See LICENSE.txt for details.
 from collections.abc import MutableMapping
 
+class URI (str):
+    """URI string
+
+    This is exactly like a str object (and will even test as equal to
+    strings of identical value). It is a literal instance of str.
+
+    The only difference is that these also return true as instances of
+    URI, which is a distinct type of XMP value.
+
+        >>> isinstance(URI("hello"), str)
+        True
+        >>> isinstance(URI("hello"), URI)
+        True
+        >>> isinstance("hello", URI)
+        False
+        >>> URI("hello") == "hello"
+        True
+        >>> "hello" == URI("hello")
+        True
+
+    Other than pass the "is this a URI" test, URI objects can't do
+    anything that str objects can't do.
+    """
+
+    def __new__ (cls, *args, **kwargs):
+        """Return a new str object."""
+        return str.__new__(cls, *args, **kwargs)
+
+    def __repr__ (self):
+        """Return a str representing a URI (rather than a str)."""
+        return "<{} {}>".format(self.__class__.__name__, self)
+
 class VanillaXMP (MutableMapping):
+
     def __delitem__ (self, key):
         pass
 
