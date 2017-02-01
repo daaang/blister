a3c3c5f4cbbea80aada1358ca52c698cf13136cc
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-01 11:41:09 -0500

    Move XMP init into setUp()

--- unittests/test_xmp.py
+++ unittests/test_xmp.py
@@ -9,19 +9,20 @@ from blister.xmp import XMP
 
 class XMPTest (unittest.TestCase):
 
+    def setUp (self):
+        self.xmp = XMP()
+
     def test_degenerate (self):
-        xmp = XMP()
-        assert_that(xmp, evaluates_to(False))
-        assert_that(xmp, has_length(0))
-        assert_that(list(xmp), is_(equal_to([])))
+        assert_that(self.xmp, evaluates_to(False))
+        assert_that(self.xmp, has_length(0))
+        assert_that(list(self.xmp), is_(equal_to([])))
 
     def test_not_all_attrs_exist (self):
-        xmp = XMP()
-        assert_that(calling(getattr).with_args(xmp, "fake_namespace"),
+        assert_that(calling(getattr).with_args(self.xmp,
+                                               "fake_namespace"),
                     raises(AttributeError))
-        assert_that(calling(getattr).with_args(xmp, "also_fake"),
+        assert_that(calling(getattr).with_args(self.xmp, "also_fake"),
                     raises(AttributeError))
 
     def test_default_xmp_namespaces_exist (self):
-        xmp = XMP()
-        no_error = xmp.stRef
+        no_error = self.xmp.stRef
