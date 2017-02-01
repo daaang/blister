da4f7a97eaa0c79739c089f77f589ebd789e5eb7
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-01 17:17:50 -0500

    Rewrite getattr to call getitem

--- lib/blister/xmp.pyx
+++ lib/blister/xmp.pyx
@@ -22,19 +22,19 @@ cdef class XMP:
     def __iter__ (self):
         return iter(())
 
-    def __getattr__ (self, name):
-        if name in DEFAULT_XMP_NAMESPACES:
-            return ()
-
-        else:
-            self.raise_error_no_attr(name)
-
     def __getitem__ (self, key):
         if key in DEFAULT_XMP_NAMESPACES:
             return ()
 
         else:
             raise KeyError(key)
+
+    def __getattr__ (self, name):
+        try:
+            return self[name]
+
+        except KeyError:
+            self.raise_error_no_attr(name)
 
     def __repr__ (self):
         return "<{}>".format(self.__class__.__name__)
