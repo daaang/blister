c587402b53a8c867bc2f11624264e58c3bcf696b
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-01 10:58:45 -0500

    XMP is a class

--- lib/blister/xmp.pyx
+++ lib/blister/xmp.pyx
@@ -1,12 +1,6 @@
 # Copyright (c) 2017 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
-from hamcrest import *
-import unittest
 
-from blister.xmp import XMP
-
-class XMPTest (unittest.TestCase):
-
-    def test_degenerate (self):
-        xmp = XMP()
+class XMP:
+    pass
